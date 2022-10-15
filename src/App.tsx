a8ffdfecdd99f75fcd4bf8501d5f8d888b6c0f54
Lyubov Korovina <lukorovina@gmail.com>
2022-10-15 18:41:01 -0400

    added home content component

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ function App() {
         <div className='container'>
           <Routes>
             <Route path='/' element={<Home/>}/>
+            <Route path='/home' element={<Home/>}/>
           </Routes>
         </div>
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,30 +1,31 @@
 import React from "react";
+import { NavLink } from 'react-router-dom';
 
 function Header()
 {
     return(
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a className="navbar-brand" href="#">ANORGEOUS</a>
+            <NavLink to={"/home"} className="navbar-brand">ANORGEOUS</NavLink>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav mr-auto mb-2 mb-lg-0">
                 <li className="nav-item active">
-                  <a className="nav-link" aria-current="page" href="index.html"><i className="fa-solid fa-house fa-lg"></i>Home<span className="sr-only">(current)</span></a>
+                  <NavLink to={"/home"} className="nav-link" aria-current="page"><i className="fa-solid fa-house fa-lg"></i>Home<span className="sr-only">(current)</span></NavLink>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" aria-current="page" href="about.html"><i className="fa-solid fa-circle-info fa-lg"></i>About</a>
+                  <NavLink to={"/about"} className="nav-link" aria-current="page"><i className="fa-solid fa-circle-info fa-lg"></i>About</NavLink>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" aria-current="page" href="apps.html"><i className="fa-solid fa-mobile-button fa-lg"></i>Apps</a>
+                    <NavLink to={"/apps"} className="nav-link" aria-current="page"><i className="fa-solid fa-mobile-button fa-lg"></i>Apps</NavLink>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" aria-current="page" href="services.html"><i className="fa-solid fa-signs-post fa-lg"></i>Services</a>
+                    <NavLink to={"/services"} className="nav-link" aria-current="page"><i className="fa-solid fa-signs-post fa-lg"></i>Services</NavLink>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" aria-current="page" href="contact.html"><i className="fa-solid fa-paper-plane fa-lg"></i>Contact Us</a>
+                    <NavLink to={"/contact"} className="nav-link" aria-current="page"><i className="fa-solid fa-paper-plane fa-lg"></i>Contact Us</NavLink>
                   </li>
               </ul>
             </div>
